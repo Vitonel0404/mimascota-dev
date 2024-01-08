12e93e78e149ff46e303e900bdba8c2c80abdb0a
Vitonel0404 <victor0404.vmhm@gmail.com>
2024-01-08 15:47:34 -0500

    first commit 5

--- veterinaria/settings/local.py
+++ veterinaria/settings/local.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'veterinaria-demo',
+        'NAME': 'veterinaria_demo',
         'USER': 'postgres',
         'PASSWORD': 'ydaleu',
         'HOST':'159.223.186.222',
