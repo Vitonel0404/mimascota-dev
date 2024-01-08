ad9f60a3960f1b6a97dd2d2c0f79920c6938d0ea
Vitonel0404 <victor0404.vmhm@gmail.com>
2024-01-08 15:34:12 -0500

    first commit 2

--- veterinaria/settings/local.py
+++ veterinaria/settings/local.py
@@ -6,13 +6,23 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vettypet-dev',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST':'localhost',
+#         'DATABASE_PORT':'5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vettypet-dev',
+        'NAME': 'veterinaria-demo',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST':'localhost',
+        'PASSWORD': 'ydaleu',
+        'HOST':'159.223.186.222',
         'DATABASE_PORT':'5432',
     }
 }
